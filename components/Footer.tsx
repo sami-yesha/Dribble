10d538185db8569f4efb4c27ed31baf3152e6ae5
sami <samuelyeshambel9@gmail.com>
2024-03-21 09:37:11 +0300

    .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
-
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+type ColumnPrpos={
+  title:string;
+  links:Array<string>;
+}
+const FooterColumn=({title,links})=>(
+  <div className="footer_column">
+           <h4 className="font-semibold">{title}</h4>
+           <ul className="flex flex-col gap-2 font-normal">
+            {links.map((link)=> <Link href='/' key={link}>{link}</Link>)}
+           </ul>
+  </div>
+)
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt="Flexibble"
+          />
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            Flexibble is the world's leading community for creatives to share , grow, and get hired.
+          </p>
+        </div>
+        <div className="flex flex-wrao gap-12">
+          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links}/>
 
-export default Footer
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

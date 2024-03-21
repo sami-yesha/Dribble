26ff7de1013d78282aa676ac691a2887ae07644d
sami <samuelyeshambel9@gmail.com>
2024-03-21 09:00:33 +0300

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import Link from 'next/link'
+import Image from 'next/image'
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+             <Image 
+                 src='/logo.svg'
+                 width={115}
+                 height={43}
+                 alt='flexibble'
+             />
+        </Link> 
+      </div>
+    </nav>
   )
 }
 

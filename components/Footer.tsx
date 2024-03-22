1cb0dce47b30beb8a1793f1714a6ee033e35cc52
sami <samuelyeshambel9@gmail.com>
2024-03-22 22:03:38 +0300

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,18 +2,22 @@ import { footerLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-type ColumnPrpos={
-  title:string;
-  links:Array<string>;
-}
-const FooterColumn=({title,links})=>(
+type ColumnPrpos = {
+  title: string;
+  links: Array<string>;
+};
+const FooterColumn = ({ title, links }:ColumnPrpos) => (
   <div className="footer_column">
-           <h4 className="font-semibold">{title}</h4>
-           <ul className="flex flex-col gap-2 font-normal">
-            {links.map((link)=> <Link href='/' key={link}>{link}</Link>)}
-           </ul>
+    <h4 className="font-semibold">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
   </div>
-)
+);
 const Footer = () => {
   return (
     <footer className="flexStart footer">
@@ -26,12 +30,15 @@ const Footer = () => {
             alt="Flexibble"
           />
           <p className="text-start text-sm font-normal mt-5 max-w-xs">
-            Flexibble is the world's leading community for creatives to share , grow, and get hired.
+            Flexibble is the world's leading community for creatives to share ,
+            grow, and get hired.
           </p>
         </div>
         <div className="flex flex-wrao gap-12">
-          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links}/>
-
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
         </div>
       </div>
     </footer>
